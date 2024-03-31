b06e82a47cfbcfb0483f404153c6653b4198eec0
Caio Camatta <caioz.co@gmail.com>
2024-03-31 13:19:49 -0400

    Comment

--- src/parser.c
+++ src/parser.c
@@ -447,7 +447,7 @@ static Expression* blockExpression(ASTParser* parser) {
     }
     consume(parser, TOKEN_RIGHT_CURLY, "Unclosed block expression. Expected '}'.");
 
-    // Block expressions must have at least one statement in them.
+    // Block expressions must have at one expression in them.
     if (!blockExpr->statementArray.used && !blockExpr->lastExpression) errorAtCurrent(parser, "Encountered an empty block-expression.");
 
     // Make the last ExpressionStatement the block's `lastExpression`
