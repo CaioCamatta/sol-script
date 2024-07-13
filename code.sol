7514df5b029a0a8c42a91c1fc854840877710988
Caio Camatta <caioz.co@gmail.com>
2024-07-13 10:15:52 -0400

    Nested calls work woot woot

--- code.sol
+++ code.sol
@@ -1,3 +1,8 @@
-val a = lambda(b){print b;};
-a(1);
+val a = lambda(b,c){print b+c;};
+a({2;}, 3);
+a({12;}, 13);
+
+val b = lambda(b,c){b+c;};
+print b(b({12;}, 13), 2);
+
 //val a = lambda(){print 1;};
