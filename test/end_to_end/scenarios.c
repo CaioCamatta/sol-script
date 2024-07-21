69e079876a17b5542a519649a588d28e45e5821d
Caio Camatta <caioz.co@gmail.com>
2024-07-21 09:44:46 -0400

    add macro for scenarios

--- test/end_to_end/scenarios.c
+++ test/end_to_end/scenarios.c
@@ -7,10 +7,10 @@
 
 #define MAX_OUTPUT_SIZE 1024
 
-char captured_output[MAX_OUTPUT_SIZE];
+char capturedOutput[MAX_OUTPUT_SIZE];
 
 // Function to capture stdout
-static int capture_stdout(const char* input, char* output, size_t output_size) {
+static int captureStdout(const char* input, char* output, size_t outputSize) {
     FILE* temp_stdout = tmpfile();
     if (!temp_stdout) return -1;
 
@@ -23,21 +23,25 @@ static int capture_stdout(const char* input, char* output, size_t output_size) {
     stdout = original_stdout;
 
     rewind(temp_stdout);
-    size_t bytes_read = fread(output, 1, output_size - 1, temp_stdout);
+    size_t bytes_read = fread(output, 1, outputSize - 1, temp_stdout);
     output[bytes_read] = '\0';
 
     fclose(temp_stdout);
     return 0;
 }
 
+#define SCENARIO(solCode, expectedOutput)                         \
+    do {                                                          \
+        const char* input = solCode;                              \
+        captureStdout(input, capturedOutput, MAX_OUTPUT_SIZE);    \
+        ASSERT(strcmp(capturedOutput, expectedOutput "\n") == 0); \
+        return 0;                                                 \
+    } while (0);
+
 static int test_multiply_and_add() {
-    const char* input =
-        "val multiply = lambda (a, b) { a * b; };\n"
-        "val add = lambda (a, b) { a + b; };\n"
-        "print add(multiply(2, 3), multiply(4, 5));\n";
-
-    capture_stdout(input, captured_output, MAX_OUTPUT_SIZE);
-
-    ASSERT(strcmp(captured_output, "26.000000\n") == 0);
-    return 0;
+    SCENARIO(
+        "val multiply = lambda (a, b) { a * b; };"
+        "val add = lambda (a, b) { a + b; };"
+        "print add(multiply(2, 3), multiply(4, 5));",
+        "26.000000");
 }
