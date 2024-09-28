52947326e82edf0ac5a9d6c984a69f5c802f4808
Caio Camatta <caioz.co@gmail.com>
2024-09-28 11:31:14 -0400

    Add example that breaks REPL

--- code.sol
+++ code.sol
@@ -1,14 +1,3 @@
-var array = struct { 
-    length: 3;
-    get: lambda(index) {
-        if (index == 0) { return 10; }
-        else if (index == 1) { return 20; }
-        else if (index == 2) { return 30; }
-        "OutOfBounds"; 
-    };
-};
-print array.length;
-print array.get(0);
-print array.get(1);
-print array.get(2);
-print array.get(3); 
+var car = struct { year: 2020; };
+car.year;
+car.year;
